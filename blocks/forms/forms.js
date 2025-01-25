fe8c498f1c7c50420890b0eb2bd8bb91456e3767
Krizel Minnema <krizelminnema@pm.me>
2025-01-24 18:18:26 -0800

    added embedForm for marketo for testing

--- blocks/forms/forms.js
+++ blocks/forms/forms.js
@@ -1,1 +1,10 @@
-  const MUNCHKIN = '435-TDP-284';
+const MUNCHKIN = '435-TDP-284';
+const FORMS2 = '//www2.mammotome.com/js/forms2/js/forms2.min.js';
+
+const embedForm = (formid) => {
+  `<script src="${FORMS2}"></script>
+      <form id="mktoForm_${formid}">
+      </form>
+      <script>MktoForms2.loadForm("//www2.mammotome.com", "${MUNCHKIN}", (${formid});</script>`;
+    return embedForm;
+};
