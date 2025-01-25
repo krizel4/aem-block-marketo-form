998cda1fe9962e632ffbe30c0b4d5698cc25bc4b
Krizel Minnema <krizelminnema@pm.me>
2025-01-24 18:21:25 -0800

    removed forms2 variable

--- blocks/forms/forms.js
+++ blocks/forms/forms.js
@@ -1,8 +1,7 @@
 const MUNCHKIN = '435-TDP-284';
-const FORMS2 = '//www2.mammotome.com/js/forms2/js/forms2.min.js';
 
 const embedForm = (formid) => {
-  `<script src="${FORMS2}"></script>
+  `<script src="//www2.mammotome.com/js/forms2/js/forms2.min.js"></script>
       <form id="mktoForm_${formid}">
       </form>
       <script>MktoForms2.loadForm("//www2.mammotome.com", "${MUNCHKIN}", (${formid});</script>`;
