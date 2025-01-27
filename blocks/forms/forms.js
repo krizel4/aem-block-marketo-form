c78d4b574acdefbd9bb4ee726a95f8b08dda8574
Krizel Minnema <krizelminnema@pm.me>
2025-01-26 16:55:02 -0800

    cleaned up embedMarketoForm

--- blocks/forms/forms.js
+++ blocks/forms/forms.js
@@ -1,13 +1,8 @@
 
-const embedMarketoForm = (formId) => {
-formContainer.innerHTML = `<script src="//www2.mammotome.com/js/forms2/js/forms2.min.js"></script> <form id="mktoForm_1063"></form> <script>MktoForms2.loadForm("//www2.mammotome.com", "435-TDP-284", ${formId});</script>`;
-    document.body.appendChild(formContainer);
-    return embedMarketoForm;
-}
+const embedMarketoForm = (formId) =>`<script src="//www2.mammotome.com/js/forms2/js/forms2.min.js"></script> <form id="mktoForm_1063"></form> <script>MktoForms2.loadForm("//www2.mammotome.com","435-TDP-284",${formId});</script>`;
+
 
 export default function decorate(block) {
-  const link = block.dataset.link;
   const formId = block.dataset.formId || '1063'; // default form is US-EN website form
-  block.textContent = '';
-  loadEmbed(block, link, formId);
+  block.innerHTML = embedMarketoForm(formId);
 }
