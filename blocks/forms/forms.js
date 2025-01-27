bb6c4a9f962b932cbeb776c7a66f42e686f39596
Krizel Minnema <krizelminnema@pm.me>
2025-01-26 21:18:02 -0800

    taking textContent and trimming it to add formId.

--- blocks/forms/forms.js
+++ blocks/forms/forms.js
@@ -1,8 +1,21 @@
+const embedMarketoForm = (block, formId) => {
+  // Load the Marketo script
+  const marketoScript = document.createElement('script');
+  marketoScript.src = '//www2.mammotome.com/js/forms2/js/forms2.min.js';
 
-const embedMarketoForm = (formId) =>`<script src="//www2.mammotome.com/js/forms2/js/forms2.min.js"></script> <form id="mktoForm_1063"></form> <script>MktoForms2.loadForm("//www2.mammotome.com","435-TDP-284",${formId});</script>`;
+  marketoScript.onload = () => {
+    const formElement = document.createElement('form');
+    formElement.id = `mktoForm_${formId}`;
+    block.appendChild(formElement);
+    if (window.MktoForms2) {
+      MktoForms2.loadForm("//www2.mammotome.com", "435-TDP-284", formId);
+    }
+  };
+  document.head.appendChild(marketoScript);
+};
 
-
-export default function decorate(block) {
-  const formId = block.dataset.formId || '1063'; // default form is US-EN website form
-  block.innerHTML = embedMarketoForm(formId);
+export default async function decorate(block) {
+  const formId = block.textContent.trim();
+  block.textContent = '';
+  embedMarketoForm(block, formId);
 }
