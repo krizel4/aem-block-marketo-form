bca324c27f969c93a0c689116050d683e45255a7
Krizel Minnema <krizelminnema@pm.me>
2025-01-29 15:36:23 -0800

    remove all console.log testing

--- blocks/marketoform/marketoform.js
+++ blocks/marketoform/marketoform.js
@@ -20,30 +20,28 @@ const waitForMktoForms = () => new Promise((resolve) => {
 const embedMarketoForm = async (block, formId) => {
   await loadScript('//www2.mammotome.com/js/forms2/js/forms2.min.js', block);
   await waitForMktoForms();
+
   const formElement = document.createElement('form');
   formElement.id = `mktoForm_${formId}`;
   block.appendChild(formElement);
+
   window.MktoForms2.loadForm('//www2.mammotome.com', '435-TDP-284', formId);
+
   window.MktoForms2.whenReady((form) => {
     form.onSuccess((values, followUpUrl) => {
-      console.log('Form Submitted: ', values, followUpUrl);
       window.location.href = followUpUrl;
-      if (!followUpUrl) {
-        console.error('FollowUp URL is missing or invalid:', followUpUrl);
-        return false;
-      }
+
       dataLayer.push({
         event: 'marketo.success',
         'marketo.form_id': formId,
         'marketo.form_values': values,
         'marketo.follow_up_url': followUpUrl,
         eventCallback() {
-          console.log('Working on the redirect...');
           form.getFormElem().hide();
           document.location.href = followUpUrl;
-          console.log('Form submitted successfully');
         },
       });
+      return false;
     });
   });
 };
